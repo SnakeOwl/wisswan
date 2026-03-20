55c29ed258e3d62da199e1fad93d2bdd70f7a464
SnakeOwl <kolyania1@gmail.com>
2026-03-20 17:53:48 +0300

    FIX: привязки доменов для хаков, у авторизированного пользователя

--- app/Http/Requests/User/Hacks/ManageDomainsHackRequest.php
+++ app/Http/Requests/User/Hacks/ManageDomainsHackRequest.php
@@ -4,16 +4,6 @@ namespace App\Http\Requests\User\Hacks;
 
 class ManageDomainsHackRequest extends AccessHackRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        // anonimous hack has no user_id
-        return $this->route("hack")->user_id == null;
-    }
-
-
     public function rules(): array
     {
         return [
