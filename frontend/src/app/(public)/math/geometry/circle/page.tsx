29fc1b9124bd56918f65cbcf6fb812d97692b3f6
SnakeOwl <kolyania1@gmail.com>
2026-07-27 19:39:30 +0300

    теорема вписанного угла + фикс формулы

--- frontend/src/app/(public)/math/geometry/circle/page.tsx
+++ frontend/src/app/(public)/math/geometry/circle/page.tsx
@@ -22,12 +22,15 @@ export default function Page() {
             <CircleDescription />
 
             <Formulas />
-            
+
+            <Theorems />
+
             {/* <CircleCalculator /> */}
 
         </main>
     )
 }
+
 
 const Formulas = () => (
     <section id="#Formulas">
@@ -38,9 +41,9 @@ const Formulas = () => (
         <div className="flex flex-row flex-wrap gap-6">
             <div className="flex flex-col items-center">
                 <div className="text-2xl">
-                    S = &pi;<span className="text-green-700">R</span>
+                    S = &pi;<span className="text-green-700">R</span><sup>2</sup>
                 </div>
-                
+
                 <span>
                     Площадь круга
                 </span>
@@ -50,7 +53,7 @@ const Formulas = () => (
                 <div className="text-2xl">
                     C = &pi;<span className="text-blue-700">D</span> = 2&pi;<span className="text-green-700">R</span>
                 </div>
-                
+
                 <span>
                     Длина окружности
                 </span>
@@ -58,6 +61,29 @@ const Formulas = () => (
         </div>
     </section>
 )
+
+
+const Theorems = () => (
+    <section id="Theorems">
+        <ContentDividerV1 className='my-4'>
+            <h2 className="text-2xl text-nowrap">Теоремы</h2>
+        </ContentDividerV1>
+
+        <div className="flex flex-col xl:flex-row">
+            <div>
+                Вписанный угол равен половине соответствующего ему центрального угла.
+            </div>
+
+            <Image
+                src="/images/storage/geometry/circle_inner_corner.svg"
+                width={300} height={300}
+                alt="Рисунок круга на котором зелёными линиями подписан вписанный угол, а синими линиями центральный угол. Центральный угол идёт из центра, вписанный идёт из края круга."
+                unoptimized
+            />
+        </div>
+    </section>
+)
+
 
 const CircleDescription = () => (
     <section id="#CircleDescription">
@@ -75,7 +101,7 @@ const CircleDescription = () => (
             />
 
             <div className="flex flex-col gap-2">
-                <p><strong className="text-blue-700" >Диаметр</strong> — это отрезок, который соединяет две точки окружности и обязательно проходит через её центр. Иногда обозначается так: &#8960;</p> 
+                <p><strong className="text-blue-700" >Диаметр</strong> — это отрезок, который соединяет две точки окружности и обязательно проходит через её центр. Иногда обозначается так: &#8960;</p>
                 <p><strong className="text-green-700">Радиус</strong> — это отрезок, соединяющий центр окружности с любой точкой, лежащей на этой окружности.</p>
 
                 <p><strong>Окружность</strong>&nbsp;&mdash; это замкнутая линия на&nbsp;плоскости, все точки которой находятся на&nbsp;одинаковом расстоянии от&nbsp;одной точки&nbsp;&mdash; центра окружности.</p>
@@ -83,6 +109,10 @@ const CircleDescription = () => (
                 <p><strong>Хордой</strong> окружности называется отрезок соединяющий две точки окружности.</p>
                 <p><strong>Дугой</strong> окружности называется часть окружности, ограниченная двумя точками.</p>
                 <p><strong>Сектором</strong> называется часть круга, заключенная между двумя радиусами.</p>
+
+                <dl>
+                    <div><dt className="inline font-bold">Вписанный угол</dt> — <dd className="inline">угол, вершина которого лежит на окружности, а стороны пересекают эту окружность. </dd></div>
+                </dl>
             </div>
         </div>
     </section>
