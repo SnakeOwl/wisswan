5fe6576f4ef1802079a979bb0e60489275d8fae3
SnakeOwl <kolyania1@gmail.com>
2026-08-03 23:45:31 +0300

    доработка калькулятора треугольников

--- frontend/src/app/(public)/math/geometry/triangle/_components/Calculator.tsx
+++ frontend/src/app/(public)/math/geometry/triangle/_components/Calculator.tsx
@@ -16,6 +16,7 @@ import {
     InputGroupAddon,
     InputGroupInput,
 } from "@/components/ui/input-group"
+import { Button } from "@/components/ui/button";
 
 
 type triangleType = "right";
@@ -45,6 +46,9 @@ export default function TriangleCalculator() {
         let g: number = Number(newData.g) || 0;
         let f: number = Number(newData.f) || 0;
 
+        let S: number = Number(newData.S) || 0;
+        let P: number = Number(newData.P) || 0;
+
 
 
         switch (fieldName) {
@@ -61,6 +65,7 @@ export default function TriangleCalculator() {
                             f = Math.sqrt(e * e + g * g);
                         }
                     }
+
                 }
                 break;
 
@@ -82,42 +87,79 @@ export default function TriangleCalculator() {
                 break;
 
             case "e":
-                if (e > 0) {
-                    // Свойство прямоугольного треугольника: Катет прямоугольного треугольника, лежащий против угла в 30°, равен половине гипотенузы.
-                    if (ω == 30) {
-                        f = e * 2;
-                    } else if (ω == 45) {
-                        g = e;
-                        f = Math.sqrt(e * e + g * g);
+                if (triangleType == "right") {
+                    if (e > 0) {
+                        // Свойство прямоугольного треугольника: Катет прямоугольного треугольника, лежащий против угла в 30°, равен половине гипотенузы.
+                        if (ω == 30) {
+                            f = e * 2;
+                        } else if (ω == 45) {
+                            g = e;
+                            f = Math.sqrt(e * e + g * g);
+                        }
                     }
+
+                    // TODO: какая-то неудобная херня 1
+                    if (f > 0) {
+                        g = Math.sqrt(f * f - e * e);
+                    }
+                    
+
                 }
                 break;
 
             case "g":
-                if (g > 0) {
-                    // Свойство прямоугольного треугольника: Катет прямоугольного треугольника, лежащий против угла в 30°, равен половине гипотенузы.
-                    if (α == 30) {
-                        f = g * 2;
-                    } else if (α == 45) {
-                        // катеты равны
-                        e = g;
-                        f = Math.sqrt(e * e + g * g);
-                    } else if (e > 0) {
-                        // есть 2 катета чтобы просчитать гипотенузу
-                        f = Math.sqrt(e * e + g * g);
+                if (triangleType == "right") {
+                    if (g > 0) {
+                        // Свойство прямоугольного треугольника: Катет прямоугольного треугольника, лежащий против угла в 30°, равен половине гипотенузы.
+                        if (α == 30) {
+                            f = g * 2;
+                        } else if (α == 45) {
+                            // катеты равны
+                            e = g;
+                            f = Math.sqrt(e * e + g * g);
+                        } else if (e > 0) {
+                            // есть 2 катета чтобы просчитать гипотенузу
+                            f = Math.sqrt(e * e + g * g);
+                        }
                     }
-
                 }
                 break;
 
             case "f":
+                if (triangleType == "right") {
+                    if (α == 30) {
+                        g = f / 2;
+                    }
+                    if (ω == 30) {
+                        e = f / 2;
+                    }
+                }
+                // Свойство прямоугольного треугольника: Катет прямоугольного треугольника, лежащий против угла в 30°, равен половине гипотенузы.
 
 
                 break;
+        }
+
+        if (triangleType == "right") {
+            // TODO: какая-то неудобная херня 1
+            if (fieldName != "e") {
+                if (α == 30 && f > 0) {
+                    g = f / 2;
+                    e = Math.sqrt(f * f - g * g);
+                }
+
+                if (ω == 30 && f > 0) {
+                    e = f / 2;
+                    g = Math.sqrt(f * f - e * e);
+                }
+            }
+
+
+            S = (g > 0 && e > 0) ? g * e / 2 : 0;
+            P = (g > 0 && e > 0 && f > 0) ? g + e + f : 0;
 
         }
-        console.log(`α: ${α} ω: ${ω} γ: ${γ}`)
-        console.log(`e: ${e} g: ${g} f: ${f}`)
+
 
         setData({
             "α": α == 0 ? "" : Number(Number(α).toFixed(4)),
@@ -127,6 +169,9 @@ export default function TriangleCalculator() {
             "e": e == 0 ? "" : Number(Number(e).toFixed(4)),
             "g": g == 0 ? "" : Number(Number(g).toFixed(4)),
             "f": f == 0 ? "" : Number(Number(f).toFixed(4)),
+
+            "S": S == 0 ? "" : Number(Number(S).toFixed(4)),
+            "P": P == 0 ? "" : Number(Number(P).toFixed(4)),
         });
     }
 
@@ -243,13 +288,46 @@ export default function TriangleCalculator() {
                                 type="number"
                                 value={data.f || ""}
                                 onChange={e => changeInput("f", e)}
+                            />
+                        </Field>
+                    </div>
+
+                    <div className="flex flex-col gap-3">
+                        <Field>
+                            <FieldLabel htmlFor="S" className="text-lg">S (площадь)</FieldLabel>
+
+                            <Input id="S"
+                                type="number"
+                                value={data.S || ""}
+                                onChange={e => changeInput("S", e)}
                                 disabled
                             />
+                        </Field>
 
+                        <Field>
+                            <FieldLabel htmlFor="P" className="text-lg">P (периметр)</FieldLabel>
+
+                            <Input id="P"
+                                type="number"
+                                value={data.P || ""}
+                                onChange={e => changeInput("P", e)}
+                                disabled
+                            />
                         </Field>
                     </div>
                 </div>
             </section>
+
+            <div className="mt-4">
+
+                <Button
+                    onClick={() => setData({
+                        γ: 90
+                    })}
+                >
+                    Сброс
+                </Button>
+            </div>
         </section>
     )
 }
